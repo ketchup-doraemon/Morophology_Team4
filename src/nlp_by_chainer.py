0095932f1f9a2eae32fcebe8652b9bc8a1ea1912
DaisukeYoda <d.y2487@keio.jp>
2018-12-15 03:00:27 +0900

    Add files via upload

--- src/nlp_by_chainer.py
+++ src/nlp_by_chainer.py
@@ -11,141 +11,101 @@ import chainer.functions as F
 import chainer.links as L
 
 
-with open('english_brown.txt') as f:
-    data = f.read()
-    data = data.replace('.','')
-    data = data.replace(',','')
-    data = data.replace('""','')
-    data = data.lower()
-    
-all_words = data.split()
-words_set = np.unique(all_words)[727:]
-
-
-def one_hot_expression(word):
-    
-    fig_word = [ord(char) - 97 for char in word]
-    
-    return np.array(fig_word)
-    #n_class = 26
-    #return np.identity(n_class)[fig_word]
-
-
-def softmax(array):
-    return np.exp(array)/np.sum(np.exp(array))
+#The function converts the word to a figure.
+def word2fig(word):   
+    return np.array([ord(char) - 97 for char in word])
 
 
 class LSTM(Chain):
-    def __init__(self, in_size, hidden_size, out_size):
+    def __init__(self, in_size, hidden_size,hidden2_size, out_size):
         super(LSTM, self).__init__(
             xh = L.EmbedID(in_size, hidden_size),
-            #xh = L.Linear(in_size, hidden_size),
-            hh = L.LSTM(hidden_size, hidden_size),
-            hy = L.Linear(hidden_size, out_size)
-        )
+            bn = L.BatchNormalization(hidden_size),
+            hh = L.LSTM(hidden_size, hidden2_size),
+            hh2 = L.LSTM(hidden2_size, hidden2_size),
+            hy = L.Linear(hidden2_size, out_size),         
+            )
  
-    def __call__(self, x, t=None, train=False):
+    def __call__(self, x, t):
         x = Variable(x)
-        if train:
-            t = Variable(t)
+        t = Variable(t)
+        
         h = self.xh(x)
+        h = self.bn(h)
+        h = F.tanh(h)
         h = self.hh(h)
+        h = self.hh2(h)
         y = F.softmax(self.hy(h))
-        #y = F.max(y)
-        if train:
-            return F.softmax_cross_entropy(y, t)
-        else:
-            return y.data
+        
+        return F.softmax_cross_entropy(y, t)
+
+    def predict(self,x):
+        x = Variable(x)
+        
+        h = self.xh(x)
+        h = self.bn(h)
+        h = F.tanh(h)
+        h = self.hh(h)
+        h = self.hh2(h)
+        y = F.softmax(self.hy(h))
+        
+        return y.data        
  
     def reset(self):
         self.cleargrads()
         self.hh.reset_state()
         
+        
+if __name__ == '__main__':
+    
+    with open('english_brown.txt') as f:
+        data = f.read()
+        data = data.replace('.','')
+        data = data.replace(',','')
+        data = data.replace('""','')
+        data = data.lower()
+        
+    all_words = data.split()
+    words_set = np.unique(all_words)[727:]
+    
 
-model = LSTM(in_size=26, hidden_size=100, out_size=26)
+    model = LSTM(27,300,300,27)
+    optimizer = optimizers.Adam()
+    optimizer.setup(model)
+    
+    training_data = ['worker','working','workshop','works','worked','work','workers']
+    training_data = [np.hstack([word2fig(x),26]).astype(np.int32) for x in training_data]
+    
+    loss_record = []
+    for i in range(1000):
+        model.reset()
+        trainX = np.random.choice(training_data,1)[0]
+        loss = 0
+        for i in range(trainX.shape[0] - 1):    
+            x = np.array([trainX[i]])
+            t = np.array([trainX[i+1]])
+            loss += model(x=x, t=t)
+             
+        loss_record.append(int(loss.data))
+        loss.backward()
+        optimizer.update()
+        
 
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-
-trainX = one_hot_expression('worker').astype(np.int32)
-trainY = one_hot_expression('working').astype(np.int32)
-trainZ = one_hot_expression('workshop').astype(np.int32)
-trainX2 = one_hot_expression('works').astype(np.int32)
-trainY2 = one_hot_expression('worked').astype(np.int32)
-trainZ2 = one_hot_expression('work').astype(np.int32)
-
-
-for i in range(1000):
+    print('w :', 1.)
+    
+    char = np.array([trainX[0]])
     model.reset()
-    loss = 0
-    for i in range(trainX.shape[0] - 1):    
-        x = np.array([trainX[i]])
-        t = np.array([trainX[i+1]])
-        loss += model(x=x, t=t, train=True)
-         
-    loss.backward()
-    #loss.unchain_backward()
-    optimizer.update()
+    for i in range(10):
+        y = model.predict(char)
+        prob = np.max(y)
+        char = np.array([np.argmax(y)],dtype=np.int)
+        y = chr(np.argmax(y) + 97)
+        
+        print(y +' :',prob)    
+        
+    import pylab as plt
+    plt.plot(loss_record)
     
-    
-    model.reset()
-    loss = 0
-    for i in range(trainY.shape[0] - 1):    
-        x = np.array([trainY[i]])
-        t = np.array([trainY[i+1]])
-        loss += model(x=x, t=t, train=True)
-         
-    loss.backward()
-    #loss.unchain_backward()
-    optimizer.update()
-    
-    model.reset()
-    loss = 0
-    for i in range(trainZ.shape[0] - 1):    
-        x = np.array([trainZ[i]])
-        t = np.array([trainZ[i+1]])
-        loss += model(x=x, t=t, train=True)
-         
-    loss.backward()
-    #loss.unchain_backward()
-    optimizer.update()
-    
-    model.reset()
-    loss = 0
-    for i in range(trainX2.shape[0] - 1):    
-        x = np.array([trainX2[i]])
-        t = np.array([trainX2[i+1]])
-        loss += model(x=x, t=t, train=True)
-         
-    loss.backward()
-    #loss.unchain_backward()
-    optimizer.update()
-    
-    
-    model.reset()
-    loss = 0
-    for i in range(trainY2.shape[0] - 1):    
-        x = np.array([trainY2[i]])
-        t = np.array([trainY2[i+1]])
-        loss += model(x=x, t=t, train=True)
-         
-    loss.backward()
-    #loss.unchain_backward()
-    optimizer.update()
-
-
-
-print('w :', 1.)
-
-char = np.array([trainX[0]])
-model.reset()
-for i in range(5):
-    y = model(char,train=False)
-    prob = np.max(y)
-    char = np.array([np.argmax(y)],dtype=np.int)
-    y = chr(np.argmax(y) + 97)
-    
-    print(y +' :',prob)    
     
     
     
